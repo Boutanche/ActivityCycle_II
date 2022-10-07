07ead68067e5ec0da9a287b52bea0475cdbe13cc
Benoit Bertrand <benoit.bertrand@live.fr>
2022-10-08 01:05:06 +0200

    Initial Commit

--- app/src/main/java/com/example/activitycycleii/SpyActivity.java
+++ app/src/main/java/com/example/activitycycleii/SpyActivity.java
@@ -5,41 +5,75 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Classe qui permet l'espionnage sur les activités.
+ */
 public class SpyActivity extends AppCompatActivity {
+    /**
+     * Constante pour le tag : ACTIVITY CYCLE
+     */
+    private static final String TAG_AC = "ACTIVITY CYCLE";
+    /**
+     * Espion sur onCreate()
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("ACTIVITY CYCLE", "onCreate (bundle : " + savedInstanceState + ")");
+        Log.i(TAG_AC, "onCreate (bundle : " + savedInstanceState + ")");
     }
+
+    /**
+     * Espion sur onStart()
+     */
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("ACTIVITY CYCLE", "onStart");
+        Log.i(TAG_AC, "onStart");
     }
 
+    /**
+     * Espion sur onResume()
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("ACTIVITY CYCLE", "onResume");
+        Log.i(TAG_AC, "onResume");
     }
+
+    /**
+     * Espion sur onPause
+     */
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i("ACTIVITY CYCLE", "onPause");
+        Log.i(TAG_AC, "onPause");
     }
+
+    /**
+     * Espion sur onStop()
+     */
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("ACTIVITY CYCLE", "onStop");
+        Log.i(TAG_AC, "onStop");
     }
+
+    /**
+     * Espion sur onDestroy()
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i("ACTIVITY CYCLE", "onDestroy");
+        Log.i(TAG_AC, "onDestroy");
     }
+
+    /**
+     * Espion sur onRestart
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i("ACTIVITY CYCLE", "onRestart");
+        Log.i(TAG_AC, "onRestart");
     }
 }
